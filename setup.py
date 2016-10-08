2f13c675d456f9ae6d6dcdc7aa555fdf891401b6
Chris Norman <chris.norman2@googlemail.com>
2016-10-08 22:59:15 +0100

    Switched to using pbr for setup.

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
 from setuptools import setup
 
-from simpleconf import __version__
-
 setup(
- name = 'simpleconf',
- version = __version__,
- description = 'The last configuration manager I will ever write (hopefully).',
- url = 'http://github.com/chrisnorman7/simpleconf.git',
- author = 'Chris Norman',
- author_email = 'chris.norman2@googlemail.com',
- license = 'MPL',
- packages = ['simpleconf', 'simpleconf.dialogs'],
- zip_safe = False,
- keywords = ['configuration', 'config', 'saving'],
- long_description_markdown_filename = 'README.md',
- setup_requires = ['setuptools-markdown'],
- install_requires = ['six'], 
+    setup_requires = ['pbr', 'setuptools'],
+    pbr = True
 )
