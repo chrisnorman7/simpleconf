f67a1b9e841a2917915a869d362ee8507cc7e7ff
Chris Norman <chris.norman2@googlemail.com>
2017-09-08 01:01:11 +0100

    Updated example to use a tree view.

--- example.py
+++ example.py
@@ -6,9 +6,8 @@ see how it should be done.
 """
 
 import wx
-from wx.lib.sized_controls import SizedFrame
 from simpleconf import Section, Option, validators
-from simpleconf.dialogs.wx import SimpleConfWxDialog as WXDLG
+from simpleconf.dialogs.wx import SimpleConfWxPanel
 
 
 class Config(Section):
@@ -49,23 +48,62 @@ class Config(Section):
 config = Config()
 
 
-class ConfigFrame(SizedFrame):
-    """This should show buttons to load a panel for every configuration
-    section."""
+class ConfigFrame(wx.Frame):
+    """Create a tree view."""
     def __init__(self):
         super(ConfigFrame, self).__init__(None, title='Simpleconf Example')
-        p = self.GetContentsPane()
-        p.SetSizerType('form')
+        self.splitter = wx.SplitterWindow(self)
+        self.left_panel = wx.Panel(self.splitter)
+        left_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.tree = wx.TreeCtrl(self.left_panel)
+        self.Bind(wx.EVT_TREE_SEL_CHANGING, self.on_change)
+        left_sizer.Add(
+            wx.StaticText(self.left_panel, label='&Sections'),
+            0, wx.GROW
+        )
+        left_sizer.Add(self.tree, 1, wx.GROW)
+        self.left_panel.SetSizerAndFit(left_sizer)
+        self.splitter.SplitHorizontally(
+            self.left_panel, self.make_right_panel()
+        )
+        self.root = self.tree.AddRoot('Options')
+        self.tree.SetItemHasChildren(self.root)
         for s in config.sections:
             section = getattr(config, s)
-            if section.visible:
-                wx.Button(
-                    p, label='&%s' % section.title
-                ).Bind(
-                    wx.EVT_BUTTON, lambda event, section=section: WXDLG(
-                        section, self
-                    ).Show(True)
-                )
+            self.add_section(self.root, section)
+
+    def make_right_panel(self):
+        """Create a panel for the right hand side of the screen."""
+        p = wx.Panel(self.splitter)
+        s = wx.BoxSizer(wx.VERTICAL)
+        s.Add(
+            wx.StaticText(
+                p,
+                label='Select a configuration section to view the options'
+            ),
+            0, wx.GROW
+        )
+        return p
+
+    def add_section(self, parent, section):
+        """Recursively add a section to self.tree."""
+        item = self.tree.AppendItem(parent, section.title)
+        if section.sections:
+            self.tree.SetItemHasChildren(item)
+        self.tree.SetItemData(item, section)
+        for name in section.sections:
+            self.add_section(item, getattr(section, name))
+
+    def on_change(self, event):
+        item = event.GetItem()
+        section = self.tree.GetItemData(item)
+        old = self.splitter.GetWindow2()
+        if section is None:
+            new = self.make_right_panel()
+        else:
+            new = SimpleConfWxPanel(section, self.splitter)
+        self.splitter.ReplaceWindow(old, new)
+        old.Destroy()
 
 
 # Now for showing the actual dialog:
